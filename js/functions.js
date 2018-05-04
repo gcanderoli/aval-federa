19732d73c86e7797bec9da0c0d2adfe506599108
Canderoli <Gcanderoli@agea.sa>
2018-05-04 15:06:24 -0300

    first commit

---
+++ js/functions.js
@@ -0,0 +1,65 @@
+$(function () {
+
+    function whichTransitionEvent() {
+        var t,
+            el = document.createElement("fakeelement");
+
+        var transitions = {
+            "transition": "transitionend",
+            "OTransition": "oTransitionEnd",
+            "MozTransition": "transitionend",
+            "WebkitTransition": "webkitTransitionEnd"
+        }
+
+        for (t in transitions) {
+            if (el.style[t] !== undefined) {
+                return transitions[t];
+            }
+        }
+    };
+
+    var transitionEvent = whichTransitionEvent();
+    var windowWidth = $(window).width();
+
+    if (windowWidth > 575) {
+
+        $('.btn-expand-collapse, .edit-info').click(function (e) {
+            $('.primary').toggleClass('collapsed').toggleClass("col-md-12").toggleClass("col-md-3");
+            $('.secondary').toggleClass('expanded');
+            $('.terciary').removeClass('expanded');
+
+            $('.primary').one(transitionEvent, function (e) {
+                var mr = $('.secondary').css('margin-right');
+                console.log(mr);
+                if (mr == "0px") {
+                    $('.secondary').css('margin-right', "-600px");
+                } else {
+                    $('.secondary').css('margin-right', "0");
+                }
+            });
+        });
+
+
+        $('.btn-expand-collapse2, .edit-info2').click(function (e) {
+            $('.secondary').toggleClass('expanded').toggleClass("col-md-9").toggleClass("col-md-3").toggleClass('collapsed');
+            $('.terciary').toggleClass('expanded');
+        });
+    } else {
+
+        $('.btn-expand-collapse, .edit-info').click(function (e) {
+            $('.primary').toggleClass('collapsed').toggleClass("col-md-12").toggleClass("col-md-3");
+            $('.secondary').toggleClass('expanded');
+            $('.terciary').removeClass('expanded');
+
+            // $('.primary').one(transitionEvent, function(e) {
+            // var mr =  $('.secondary').css('margin-right');
+            // console.log(mr);
+            //     if (mr == "0px") {
+            //         $('.secondary').css('margin-right', "-600px");
+            //     } else {
+            //         $('.secondary').css('margin-right', "0");
+            //     }
+            //     });
+        });
+    }
+});
