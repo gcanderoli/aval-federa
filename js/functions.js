1bb9fca66c84785d8ba69e1f7ddce7044a17c9fd
Gabriel Canderoli <gabyc17@live.com.ar>
2018-05-07 01:34:56 -0300

    fixing panels effect

--- js/functions.js
+++ js/functions.js
@@ -19,47 +19,65 @@ $(function () {
     };
 
     var transitionEvent = whichTransitionEvent();
+    // console.log(transitionEvent);
     var windowWidth = $(window).width();
 
     if (windowWidth > 575) {
 
-        $('.btn-expand-collapse, .edit-info').click(function (e) {
-            $('.primary').toggleClass('collapsed').toggleClass("col-md-12").toggleClass("col-md-3");
-            $('.secondary').toggleClass('expanded');
-            $('.terciary').removeClass('expanded');
+        // $('.btn-expand-collapse, .edit-info').click(function (e) {
+        //     $('.primary').toggleClass('collapsed').toggleClass("col-md-12").toggleClass("col-md-3");
+        //     $('.secondary').toggleClass('expanded');
+        //     $('.terciary').removeClass('expanded');
 
-            $('.primary').one(transitionEvent, function (e) {
-                var mr = $('.secondary').css('margin-right');
-                console.log(mr);
-                if (mr == "0px") {
-                    $('.secondary').css('margin-right', "-600px");
-                } else {
-                    $('.secondary').css('margin-right', "0");
-                }
-            });
-        });
+        //     $('.primary').one(transitionEvent, function (e) {
+        //         var mr = $('.secondary').css('margin-right');
+        //         console.log(mr);
+        //         if (mr == "0px") {
+        //             $('.secondary').css('margin-right', "-600px");
+        //         } else {
+        //             $('.secondary').css('margin-right', "0");
+        //         }
+        //     });
+        // });
 
 
-        $('.btn-expand-collapse2, .edit-info2').click(function (e) {
-            $('.secondary').toggleClass('expanded').toggleClass("col-md-9").toggleClass("col-md-3").toggleClass('collapsed');
-            $('.terciary').toggleClass('expanded');
-        });
+        // $('.btn-expand-collapse2, .edit-info2').click(function (e) {
+        //     $('.secondary').toggleClass('expanded').toggleClass("col-md-9").toggleClass("col-md-3").toggleClass('collapsed');
+        //     $('.terciary').toggleClass('expanded');
+        // });
+
+        console.log("es mayor");
     } else {
 
         $('.btn-expand-collapse, .edit-info').click(function (e) {
-            $('.primary').toggleClass('collapsed').toggleClass("col-md-12").toggleClass("col-md-3");
-            $('.secondary').toggleClass('expanded');
-            $('.terciary').removeClass('expanded');
+            console.log("click")
+            $('.primary').toggleClass('collapsed');
 
-            // $('.primary').one(transitionEvent, function(e) {
-            // var mr =  $('.secondary').css('margin-right');
-            // console.log(mr);
-            //     if (mr == "0px") {
-            //         $('.secondary').css('margin-right', "-600px");
-            //     } else {
-            //         $('.secondary').css('margin-right', "0");
-            //     }
-            //     });
+            // $('.primary.collapsed .mobileCollapsed').addClass('effect');
+            // $('.secondary').toggleClass('expanded');
+            // $('.terciary').removeClass('expanded');
+
+
+            $('.primary.collapsed ').one(transitionEvent, function (e) {
+                console.log('end')
+                $('.primary.collapsed .mobileCollapsed').addClass('effect');
+                $('.secondary').toggleClass('expanded ');
+            });
+
         });
+
+
+
+        $('.btn-expand-collapse2, .edit-info2').click(function (e) {
+            $('.secondary').toggleClass('expanded collapsed')
+            $('.terciary').toggleClass('expanded');
+        });
+
+        $('.btn-expand-collapse3').click(function (e) {
+            $('.contact').toggleClass('expanded');
+            
+        });
+
+
     }
 });
